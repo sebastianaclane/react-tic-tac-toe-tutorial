4f0c16774a01864a6ceef8f35efd5f88a107f7a6
sebastianlane <sebastianaclane@gmail.com>
2019-01-23 18:06:00 -0800

    Overview Step Completed with ability to place X's in Squares

--- src/index.js
+++ src/index.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+class Square extends Component {
+    constructor(props) {
+      // In JavaScript classes, you need to always call super when defining the constructor of a subclass. All React component classes that have a constructor should start it with a super(props) call.
+      super(props);
+      // To “remember” things, components use state.
+      this.state = {
+        value: null
+      }
+    }
+
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        // Notice how with onClick={() => alert('click')}, we’re passing a function as the onClick prop. It only fires after a click. Forgetting () => and writing onClick={alert('click')} is a common mistake, and would fire the alert every time the component re-renders.
+
+        // By calling this.setState from an onClick handler in the Square’s render method, we tell React to re-render that Square whenever its <button> is clicked. After the update, the Square’s this.state.value will be 'X', so we’ll see the X on the game board. If you click on any Square, an X should show up.
+        // When you call setState in a component, React automatically updates the child components inside of it too.
+        <button 
+              className="square" 
+              onClick={() => this.setState({value: 'X'})}
+        >
+          {this.state.value}
         </button>
       );
     }
@@ -14,7 +30,7 @@ class Square extends React.Component {
   
   class Board extends Component {
     renderSquare(i) {
-      return <Square />;
+      return <Square value={i} />;
     }
   
     render() {
